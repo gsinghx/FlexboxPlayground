6d1e3e834218b681017b4797e2f65f110b1daeff
Gagandeep Singh <iamtherealgd@gmail.com>
2022-07-01 10:13:37 +0530

    added query params to show or hide elements

--- script.js
+++ script.js
@@ -1,29 +1,76 @@
-function changeFlexContainerProperty(className, cssPropertyName){
-  let radios = document.querySelectorAll('input.'+className);
+window.onload = function () {
+  let path = window.location.search.split("=")[1];
 
-  for(let i=0; i<radios.length; i++){
-    let selector = "."+className+" .container"
-    radios[i].addEventListener("change", function(){
-      document.querySelector("."+className+" code").innerHTML = ".container { \n &nbsp;"+className+": "+this.value+"; \n}"
+  let mainElement = document.getElementById("main");
+  let playgroundElement = document.getElementById("playground");
+  let containerElement = document.querySelector(".flex-container");
+  let containerChilddrenElements = document.querySelectorAll("#flex-container-child");
+  let itemsElement = document.querySelector(".flex-items");
+  let itemsChildrenElements = document.querySelectorAll("#flex-items-child")
+
+  switch (path) {
+    case 'flex-direction':
+    case 'align-items':
+    case 'justify-content':
+    case 'align-content':
+    case 'flex-wrap':
+      playgroundElement.style.display = 'none';
+      itemsElement.style.display = 'none';
+      document.querySelector(".flex-container h1").style.display='none';
+
+      for(let i=0; i<containerChilddrenElements.length; i++){
+        containerChilddrenElements[i].style.display = 'none';
+      }
+
+      document.querySelector("."+path).style.display='block'
+      break;
+    case 'align-self':
+    case 'flex-grow':
+    case 'flex-shrink':
+    case 'flex-order':
+      playgroundElement.style.display = 'none';
+      containerElement.style.display = 'none';
+      document.querySelector(".flex-items h1").style.display='none';
+
+      for(let i=0; i<itemsChildrenElements.length; i++){
+        itemsChildrenElements[i].style.display = 'none';
+      }
+
+      document.querySelector("."+path).style.display='block'
+      break;
+    case 'playground':
+      mainElement.style.display = 'none';
+      document.querySelector(".playground h1").style.display='none'
+      break;
+  }
+}
+
+function changeFlexContainerProperty(className, cssPropertyName) {
+  let radios = document.querySelectorAll('input.' + className);
+
+  for (let i = 0; i < radios.length; i++) {
+    let selector = "." + className + " .container"
+    radios[i].addEventListener("change", function () {
+      document.querySelector("." + className + " code").innerHTML = ".container { \n &nbsp;" + className + ": " + this.value + "; \n}"
       document.querySelector(selector).style[cssPropertyName] = this.value;
     })
   }
 }
 
-function changeFlexItemProperty(className, cssPropertyName){
-  let radios = document.querySelectorAll('input.'+className);
+function changeFlexItemProperty(className, cssPropertyName) {
+  let radios = document.querySelectorAll('input.' + className);
 
-  for(let i=0; i<radios.length; i++){
-    let selector = "."+className+" .container .two"
-    radios[i].addEventListener("change", function(){
-      document.querySelector("."+className+" code").innerHTML = ".item2 { \n &nbsp;"+className+": "+this.value+"; \n}"
+  for (let i = 0; i < radios.length; i++) {
+    let selector = "." + className + " .container .two"
+    radios[i].addEventListener("change", function () {
+      document.querySelector("." + className + " code").innerHTML = ".item2 { \n &nbsp;" + className + ": " + this.value + "; \n}"
       document.querySelector(selector).style[cssPropertyName] = this.value;
     })
   }
 }
 
 function changeFlexItemPropertyNumber(className, cssPropertyName) {
-  let selector = "."+className + " .container .item"
+  let selector = "." + className + " .container .item"
 
   let radios = document.querySelectorAll('input.' + className);
   let flexItems = document.querySelectorAll(selector);
@@ -31,7 +78,7 @@ function changeFlexItemPropertyNumber(className, cssPropertyName) {
   for (let i = 0; i < radios.length; i++) {
     radios[i].addEventListener("change", function () {
       flexItems[i].style[cssPropertyName] = this.value;
-      document.querySelector("."+className+" code").innerHTML = ".item1 { \n &nbsp;"+className+": "+this.value+"; \n}"
+      document.querySelector("." + className + " code").innerHTML = ".item1 { \n &nbsp;" + className + ": " + this.value + "; \n}"
     })
   }
 }
@@ -47,17 +94,17 @@ changeFlexItemPropertyNumber("flex-grow", "flexGrow")
 changeFlexItemPropertyNumber("flex-shrink", "flexShrink")
 changeFlexItemPropertyNumber("flex-order", "order")
 
-let playgroundProps={
-  container:{},
-  item:{}
+let playgroundProps = {
+  container: {},
+  item: {}
 }
 
-function changeFlexContainerPropertyPlayground(className, cssPropertyName){
-  let radios = document.querySelectorAll('input.playground-'+className);
+function changeFlexContainerPropertyPlayground(className, cssPropertyName) {
+  let radios = document.querySelectorAll('input.playground-' + className);
 
-  for(let i=0; i<radios.length; i++){
+  for (let i = 0; i < radios.length; i++) {
     let selector = ".playground .container"
-    radios[i].addEventListener("change", function(){
+    radios[i].addEventListener("change", function () {
       playgroundProps.container[className] = this.value;
       updatePlaygroundCode();
       document.querySelector(selector).style[cssPropertyName] = this.value;
@@ -65,12 +112,12 @@ function changeFlexContainerPropertyPlayground(className, cssPropertyName){
   }
 }
 
-function changeFlexItemPropertyPlayground(className, cssPropertyName){
-  let radios = document.querySelectorAll('input.playground-'+className);
+function changeFlexItemPropertyPlayground(className, cssPropertyName) {
+  let radios = document.querySelectorAll('input.playground-' + className);
 
-  for(let i=0; i<radios.length; i++){
+  for (let i = 0; i < radios.length; i++) {
     let selector = ".playground .container .two"
-    radios[i].addEventListener("change", function(){
+    radios[i].addEventListener("change", function () {
       playgroundProps.item[className] = this.value;
       updatePlaygroundCode();
       document.querySelector(selector).style[cssPropertyName] = this.value;
@@ -93,17 +140,17 @@ function changeFlexItemPropertyNumberPlayground(className, cssPropertyName) {
   }
 }
 
-function updatePlaygroundCode(){
+function updatePlaygroundCode() {
   let str = ".container { \n";
-  Object.keys(playgroundProps.container).forEach(key=>{
-    str+="&nbsp;"+key+" : "+playgroundProps.container[key]+"; \n"
+  Object.keys(playgroundProps.container).forEach(key => {
+    str += "&nbsp;" + key + " : " + playgroundProps.container[key] + "; \n"
   })
-  str+="} \n\n"
-  str+=".item { \n";
-  Object.keys(playgroundProps.item).forEach(key=>{
-    str+="&nbsp;"+key+" : "+playgroundProps.item[key]+"; \n"
+  str += "} \n\n"
+  str += ".item { \n";
+  Object.keys(playgroundProps.item).forEach(key => {
+    str += "&nbsp;" + key + " : " + playgroundProps.item[key] + "; \n"
   })
-  str+="} "
+  str += "} "
   document.querySelector(".playground code").innerHTML = str;
 
 }
@@ -119,18 +166,18 @@ changeFlexItemPropertyNumberPlayground("flex-grow", "flexGrow")
 changeFlexItemPropertyNumberPlayground("flex-shrink", "flexShrink")
 changeFlexItemPropertyNumberPlayground("flex-order", "order")
 
-document.getElementById("addItems").onclick = function(){
+document.getElementById("addItems").onclick = function () {
   console.log('hereee')
   let currentItems = document.querySelectorAll(".playground .container .item");
 
-  if(this.innerHTML.split(" ")[0]==="Add"){
-    for(let i=3; i<currentItems.length; i++){
+  if (this.innerHTML.split(" ")[0] === "Add") {
+    for (let i = 3; i < currentItems.length; i++) {
       currentItems[i].style.display = "block";
     }
 
     this.innerHTML = "Remove Items";
-  }else{
-    for(let i=3; i<currentItems.length; i++){
+  } else {
+    for (let i = 3; i < currentItems.length; i++) {
       currentItems[i].style.display = "none";
     }
 
